8aa7b48eef2db7591f4f0573e68b9054567d9caf
Cheren Agata <cherenads@student.ub.ac.id>
2025-04-11 14:48:46 +0700

    Create Pekerja.java

---
+++ Pekerja.java
@@ -0,0 +1,46 @@
+public class Pekerja extends Manusia {
+    private double gaji;
+    private int tahunMasuk;
+    private int jumlahAnak;
+
+    public Pekerja(double gaji, int tahunMasuk, int jumlahAnak,
+                   int hari, int bulan, int tahun,
+                   String nama, String nik, boolean jenisKelamin, boolean menikah) {
+        super(nama, nik, jenisKelamin, menikah);
+        this.gaji = gaji;
+        this.tahunMasuk = tahunMasuk;
+        this.jumlahAnak = jumlahAnak;
+    }
+
+    public double getBonus() {
+        int tahunSekarang = 2025;
+        int lamaKerja = tahunSekarang - tahunMasuk;
+        double bonus = 0;
+
+        if (lamaKerja <= 5) {
+            bonus = 0.05 * gaji;
+        } else if (lamaKerja <= 10) {
+            bonus = 0.1 * gaji;
+        } else {
+            bonus = 0.15 * gaji;
+        }
+        return bonus;
+    }
+
+    public double getGaji() {
+        return gaji;
+    }
+
+    @Override
+    public double getPendapatan() {
+        return getTunjangan() + getBonus() + gaji + (jumlahAnak * 20);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+            + "\ntahun masuk : " + tahunMasuk
+            + "\njumlah anak : " + jumlahAnak
+            + "\ngaji : " + gaji;
+    }
+}
