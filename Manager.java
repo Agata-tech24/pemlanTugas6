c738bfeaad1b8db99db745ccab04a32ef633ca46
Cheren Agata <cherenads@student.ub.ac.id>
2025-04-11 14:56:51 +0700

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,14 +1,15 @@
 public class Manager extends Pekerja {
     private String departemen;
 
-    public Manager(String departemen, double gaji, int tahun, int bulan, int tanggal, int jumlahAnak, String nama, String nik, boolean jenisKelamin, boolean menikah) {
-        super(gaji, tahun, bulan, tanggal, jumlahAnak, nama, nik, jenisKelamin, menikah);
+    public Manager(String departemen, double gaji, int tahunMasuk, int jumlahAnak,
+                   String nama, String nik, boolean jenisKelamin, boolean menikah) {
+        super(gaji, tahunMasuk, jumlahAnak, nama, nik, jenisKelamin, menikah);
         this.departemen = departemen;
     }
 
     @Override
     public double getPendapatan() {
-        return super.getPendapatan() + (0.1 * gaji);
+        return super.getPendapatan() + (0.1 * getGaji()); // getGaji() dari superclass
     }
 
     @Override
